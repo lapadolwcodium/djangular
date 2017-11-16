5db590edb3b2e089d5db83e58f10cf640d8b617f
ton <Lapadol.w@codium.co>
2017-11-16 14:34:05 +0700

    no extension

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,27 +3,16 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //template: `<h1>{{ title }}</h1><p>{{ description }} is cool </p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'Hello srvup 2!';
-  description = 'A new app';
-  query: string;
-  private routeSub:any;
 
+  constructor( ){
 
-  constructor(private route:ActivatedRoute){
-      this.routeSub = route.params.subscribe(params=>{
-          console.log(params)
-          this.query = params['q']
-      })
   }
 
   ngOnInit() {
       
   }
-  ngOnDestroy(){
-      this.routeSub.unsubscribe()
-  }
+
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,52 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { Http } from '@angular/http';
-
-
-import { VideoItem } from '../videos/video';
-import { VideoService } from '../videos/videos.service';
-
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {VideoService} from '../videos/videos.service';
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  providers: [VideoService]
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    providers: [VideoService]
 })
 export class HomeComponent implements OnInit, OnDestroy {
-    private req: any;
-   homeImageList:[VideoItem] = [] as [VideoItem]
-   videoListDefaultImage = 'assets/images/videos/1.jpg'
-  constructor(private http:Http, private router:Router, private _video:VideoService) { }
 
-  ngOnInit() {
-      this.req = this._video.list().subscribe(data=>{
-          //console.log(data.json())
-          //this.homeImageList 
-          data.filter(item=>{
-              if(item.featured){
-                let dataItem = item
-                  this.homeImageList.push(dataItem)
-              }
-          })
-          // this.homeImageList = data.json();
-       })
-  }
+    constructor() {
+    }
 
-  ngOnDestroy(){
-      this.req.unsubscribe()
-  }
+    public textInp: string = "test text inp";
 
-  preventNormal(event:MouseEvent, image:any){
-      if (!image.prevented){
-          event.preventDefault()
-          //image.setAttribute("href", "/videos")
-          //image.link = '/videos'
-          //image.prevented = true;
-          this.router.navigate(['./videos'])
-          
-      }
-  }
+    ngOnInit() {
+
+    }
+
+    ngOnDestroy() {
+
+    }
+
 
 }
