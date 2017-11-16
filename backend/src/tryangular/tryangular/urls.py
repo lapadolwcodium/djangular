8220e430dac45b13b33a90e1a770ccdf6dd3a392
ton <Lapadol.w@codium.co>
2017-11-16 12:02:45 +0700

    add ang_home.html

--- backend/src/tryangular/tryangular/urls.py
+++ backend/src/tryangular/tryangular/urls.py
@@ -19,6 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # url(r'^api/', admin.site.urls),
     url(r'^.*', TemplateView.as_view(template_name="ang_home.html"), name='home')
 
 ]
