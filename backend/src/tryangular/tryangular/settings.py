8e45afa4dbd39c540788a74091bf6364eb728421
ton <Lapadol.w@codium.co>
2017-11-20 13:13:44 +0700

    update can build

--- backend/src/tryangular/tryangular/settings.py
+++ backend/src/tryangular/tryangular/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '_x_5zi7z=188rpq!27&+afmawphid8ad78m@$iv8azciud5q@('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
